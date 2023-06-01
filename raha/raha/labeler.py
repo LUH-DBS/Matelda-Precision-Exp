5a1772296acc3c633a0ff5af5a390f41003d24a6
Malte <m.f.kuhlmann@gmail.com>
2023-06-01 18:52:45 +0200

    added the functionality for labeling by hand

--- raha/raha/labeler.py
+++ raha/raha/labeler.py
@@ -28,8 +28,23 @@ class Labeler(Sampler):
             if d.has_ground_truth:
                 self.label_with_ground_truth(d)
             else:
-                # TODO implement user dialog
-                pass
+                print("Label the dirty cells in the following sampled tuple.")
+                row = d.dataframe.iloc[d.sampled_tuple, :]
+                for j in range(d.dataframe.shape[1]):
+                    cell = (d.sampled_tuple, j)
+                    value = d.dataframe.iloc[cell]
+                    print("\n############")
+                    print(row)
+                    correction = input(
+                        "What is the correction for value '{}'? Type in the same value if it is not erronous.\n".format(
+                            value))
+                    user_label = 1 if value != correction else 0
+
+                    word = "faulty" if bool(user_label) else "correct"
+                    print(f"Labeled as {word}")
+
+                    d.labeled_cells[cell] = [user_label, correction]
+                d.labeled_tuples[d.sampled_tuple] = 1
         self.calc_number_of_labels(d)
         return d
 
