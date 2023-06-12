479eb032aba05318760c2de00c99e4425e6d73b6
Malte Kuhlmann <kuhlmann@dbs.uni-hannover.de>
2023-06-13 01:10:50 +0200

    added scripts to sample data lake and setup experiment dirs

---
+++ setup_experiments.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pathlib import Path
+
+
+
+df = pd.read_csv(Path("/home/malte/EDS-Baselines/datalake_index.csv").resolve())
+wd = Path.cwd().joinpath("experiments")
+wd.mkdir(exist_ok=True)
+
+print(f"Move experiment datasets to {wd}")
+
+for index,row in df.iterrows():
+    path = Path(row["Path"]).resolve()
+    tmp_df = pd.read_csv(path)
+    tmp_path = wd.joinpath(path.stem)
+    tmp_path.mkdir(exist_ok=True)
+    tmp_path = tmp_path.joinpath(path.name)
+    tmp_df.to_csv(tmp_path)
+
+    
