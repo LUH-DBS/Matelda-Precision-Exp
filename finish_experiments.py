39dba68ade387975c2dfd607dfb629f0f5be8b24
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2023-06-13 12:24:45 +0200

    added draft for experiment evaluation script

---
+++ finish_experiments.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import raha
+from raha.raha.predictor import Predictor
+from raha.raha.sampler import Sampler
+import tqdm
+
+path_to_experiments = Path("/home/malte/EDS-Baselines/experiments").resolve() # change here to change root repository for experiments
+n_experiments = 1 # change here to run experiments n times -> generate n states with different tuples sampled
+
+for experiment in tqdm.tqdm(path_to_experiments.iterdir()):
+    
+    if experiment.is_file():
+        continue
+    elif experiment.is_dir():
+        for file in experiment.iterdir():
+            if file.is_dir():
+                state_path = file.joinpath("state")
+                for state in state_path.iterdir():
+                        predictor = Predictor()
+                        dd = predictor.load_state(state)
+                        detection_dictionary, labeled_cells, actuall_errors_dict = predictor.run(dd)
