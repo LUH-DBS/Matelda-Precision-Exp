26d227058883feab4bbb3b0dd1f933659946928c
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2023-09-29 13:07:49 +0200

    added script to retreive lables from raha

--- calculate_precision.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import pandas as pd
-
-sampled_errors = Path("X:\\Projekte\\EDS-Baselines\\raha\\datasets\\sampled_errors.csv").resolve()
-
-df = pd.read_csv(sampled_errors)
-precision = df["IsError"].sum()/len(df)
-print(f"Precision: {precision}")
---
+++ experiment/post-processing/retrieve_labels.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import pandas as pd
+from raha.raha.predictor import Predictor
+
+path_to_lake = Path("/home/malte/EDS-Baselines/experiments_2_lables_checked").resolve()
+
+#states = []
+for experiment in path_to_lake.iterdir():
+    name = experiment.name
+    experiment = experiment.joinpath(f"raha-baran-results-{name}")
+    experiment = experiment.joinpath("state")
+    if experiment.exists():
+        for state in experiment.iterdir():
+            predictor = Predictor()
+            state = predictor.load_state(state)
+            print(state.labeled_cells)
+
+            df = pd.DataFrame(columns=["Dataset", "Row", "Column", "Value", "Ground Truth", "Label"])
+
+            for key in state.labeled_cells:
+                df.loc[len(df)] = {"Dataset": str(experiment.parent.parent),
+                                   "Row": key[0],
+                                   "Column": key[1],
+                                   "Value": state.dataframe.iloc[key],
+                                   "Ground Truth": state.labeled_cells[key][1],
+                                   "Label": state.labeled_cells[key][0]}
+
+
+            dataframe_path = experiment.parent.joinpath("labled_values.csv")
+            df.to_csv(dataframe_path, index=False)
+
+            #states.append(state)
